d993e7ae0aef368879bc19580cb415b6ae4ebee4
Leandro Nascimento <leandro.nascimento@radixeng.com.br>
2022-06-18 02:55:21 -0300

    ajuste do tamanho do vídeo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { useApp } from '@/context/AppContext';
 import { Loader } from '@/components/common/Loader';
 import { Layout } from '@/components/common/Layout';
 import { useAuth } from '@/context/AuthContainer';
+import ReactPlayer from 'react-player';
 
 export default function Home() {
   const app = useApp();
@@ -38,37 +39,13 @@ export default function Home() {
 
               <div className={styles.last}>
                 <strong>Retiro Umobi 2021</strong>
-                <YouTube
-                  videoId='wV3TQjDUdW0'
-                  opts={opts}
-                  onReady={onPlayerReady} />
+                <div>
+                  <ReactPlayer url={process.env.NEXT_PUBLIC_YOUTUBE_PLAYER} />
+                </div>
               </div>
             </div>
           </Layout>
         )}
     </>
-    //   {app.isLoading ? (
-    //     <Loader loading={app.isLoading} />
-    //   ) : (
-    //     <Layout>
-    //       <div className={styles.container}>
-    //         <span className={styles.hello}>Olá,</span>
-    //         <strong className={styles.title}>Bem-vindo à plataforma de<br /> inscrições para os Retiros da <br />Umobi</strong>
-    //         <span className={styles.subtitle}>A plataforma visa facilitar as inscrições e agilizar o processo para <br />que fique de forma independente.</span>
-
-    //         <ALink label='Quero me inscrever' path='/registration' />
-
-    //         <div className={styles.last}>
-    //           <strong>Retiro Umobi 2021</strong>
-    //           <YouTube
-    //             videoId='wV3TQjDUdW0'
-    //             opts={opts}
-    //             onReady={onPlayerReady} />
-    //         </div>
-    //       </div>
-    //     </Layout>
-    //   )}
-
-    // </>
   )
 }
